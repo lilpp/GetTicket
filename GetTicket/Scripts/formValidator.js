c62861b04cb0b92c0a6bc917de1c35c5c9d78def
klmosgame <klmosgame@gmail.com>
2017-07-01 05:31:29 +0200

    Even creator frontend v 0.5

---
+++ GetTicket/Scripts/formValidator.js
@@ -0,0 +1,6 @@
+﻿function isAfter(startDate, endDate) {
+    if (startDate > endDate) {
+        $("#not-valid_startdate").text = "A kezdés dátuma nem lehet később a befejezésénél."
+        $("#not-valid_enddate").text = "A befejezés dátuma nem lehet előbb a kezdésnél."
+    }
+}
--- GetTicket/ViewModel/EventCreateViewModel.cs
+++ GetTicket/ViewModel/EventCreateViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,20 @@ namespace GetTicket.ViewModel
     public class EventCreateViewModel
     {
         public int ID { get; set; }
+        [Display(Name = "Név")]
         public string Name { get; set; }
+        [Display(Name = "Esemény kezdete")]
+        [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
+        [Display(Name = "Esemény vége")]
+        [DataType(DataType.Date)]
         public DateTime EndDate { get; set; }
+        [Display(Name = "Jegyárúsítás vége")]
+        [DataType(DataType.Date)]
         public DateTime PurcheEnd { get; set; }
+        [Display(Name = "Esemény leírása")]
         public string Description { get; set; }
+        [Display(Name = "Esemény logó")]
         public string LogoImgUrl { get; set; }
     }
 }
